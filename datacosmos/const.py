d386010786ac1faa8aabbd6f42b735c7e12d9fcc
Mike Roberts <mike.roberts@open-cosmos.com>
2023-05-11 10:57:56 +0100

    fix: ignore bandit false positive on token URL

--- datacosmos/const.py
+++ datacosmos/const.py
@@ -4,7 +4,7 @@ from enum import Enum
 
 DATACOSMOS_PRODUCTION_BASE_URL = "https://app.open-cosmos.com/api/data/v0"
 DATACOSMOS_PRODUCTION_AUDIENCE = "https://beeapp.open-cosmos.com"
-DATACOSMOS_TOKEN_URL = "https://opencosmos.eu.auth0.com/oauth/token"
+DATACOSMOS_TOKEN_URL = "https://opencosmos.eu.auth0.com/oauth/token"  # nosec: B105
 
 
 class Satellites(Enum):
