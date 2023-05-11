7a99a3d8cd568ecea2dedac988174b3d1832fec5
Mike Roberts <mike.roberts@open-cosmos.com>
2023-05-11 19:46:29 +0100

    feat: export main classes from top level module

--- datacosmos/__init__.py
+++ datacosmos/__init__.py
@@ -1,1 +1,6 @@
 """A library for interacting with DataCosmos from Python code."""
+
+from datacosmos.auth import DataCosmosCredentials
+from datacosmos.datacosmos import DataCosmos
+
+__all__ = ["DataCosmos", "DataCosmosCredentials"]
