ee6ceb012249882ff2b07b01bad200d61dc80f12
Mike Roberts <mike.roberts@open-cosmos.com>
2023-05-11 19:40:24 +0100

    feat: add constants for production URLs

---
+++ datacosmos/const.py
@@ -0,0 +1,5 @@
+"""Stores constants used in the datacosmos package."""
+
+DATACOSMOS_PRODUCTION_BASE_URL = "https://app.open-cosmos.com/api/data/v0"
+DATACOSMOS_PRODUCTION_AUDIENCE = "https://beeapp.open-cosmos.com"
+DATACOSMOS_TOKEN_URL = "https://opencosmos.eu.auth0.com/oauth/token"
