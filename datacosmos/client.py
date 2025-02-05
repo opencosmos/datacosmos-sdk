84ea9da84c9aec66fcd6ea1647a815fa8dc13ba4
tiago.peres.sousa <tiago.peres.sousa@open-cosmos.com>
2025-02-05 11:31:18 +0000

    Apply changes in the pipeline

--- datacosmos/client.py
+++ datacosmos/client.py
@@ -9,8 +9,7 @@ from typing import Any, Optional
 
 import requests
 from oauthlib.oauth2 import BackendApplicationClient
-from requests.exceptions import (ConnectionError, HTTPError, RequestException,
-                                 Timeout)
+from requests.exceptions import ConnectionError, HTTPError, RequestException, Timeout
 from requests_oauthlib import OAuth2Session
 
 from config.config import Config
